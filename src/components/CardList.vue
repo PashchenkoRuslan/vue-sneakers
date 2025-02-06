f6b6d51218b1dadc9f08b91f781d7468d955b824
ruslanpyou <ruslanpyou@gmail.com>
2025-02-06 16:38:12 +0500

    fix: fix mobile adaptation

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -10,7 +10,7 @@ const emit = defineEmits(['addToFavorite', 'addToCart'])
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-5" v-auto-animate>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5" v-auto-animate>
     <Card
       v-for="item in items"
       :key="item.id"
